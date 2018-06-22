2284298226a25d8eb8aa2309ee942e41e71ccb39
Satoshi Ebisawa <e.satoshi@gmail.com>
2018-06-22 19:22:01 +0900

    check ENV

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
 	fmt.Println("Hello, world")
+	repos, exists := os.LookupEnv("GITHUB_REPOSITORY")
+	if !exists {
+		log.Fatalln("Please set GITHUB_REPOSITORY.")
+	}
+	fmt.Println("REPOSITORY: " + repos)
 }
